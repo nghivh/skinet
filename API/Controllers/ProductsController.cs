7515dff572b3e274e122c4326a1986db6e0c15d1
Nghi Vo Huu <nghivh@vn.fujikura.com>
2021-08-13 21:57:47 +0700

    InitialCommit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<Product>> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            var product = await context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}

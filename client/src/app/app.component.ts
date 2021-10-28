09d7dda9c6c8c3208c4e62cfaf8dbd0c772ec772
Nghi Vo Huu <nghivh@vn.fujikura.com>
2021-10-28 14:20:43 +0700

    End of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
 
@@ -11,8 +12,19 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(){}
+  constructor(private basketService: BasketService){}
 
   ngOnInit(): void {
+    const basketid = localStorage.getItem('basket_id');
+    if(basketid){
+      this.basketService.getBasket(basketid).subscribe(() => {
+        console.log("Initialised basket");
+      }, error => {
+        console.log(error);
+      });
+    }
+    else{
+
+    }
   }
 }
